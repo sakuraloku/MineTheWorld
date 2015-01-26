0d8f6e73c131027d6a5fdafdccf0f932fcbbd599
sakuraloku <sakuraloku@users.noreply.github.com>
2015-01-26 10:50:48 +0800

    Create News.java

---
+++ samples/MineYahooTextNews/structure/News.java
@@ -0,0 +1,23 @@
+package structure;
+
+public class News {
+	private String title,text;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	
+}
