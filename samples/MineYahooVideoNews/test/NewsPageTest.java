bb27448b06c69cc3886d5807aa14cb144abd6e79
sakuraloku <sakuraloku@users.noreply.github.com>
2015-01-26 10:44:42 +0800

    Update NewsPageTest.java

--- samples/MineYahooVideoNews/test/NewsPageTest.java
+++ samples/MineYahooVideoNews/test/NewsPageTest.java
@@ -5,8 +5,6 @@ import page.NewsPage;
 
 public class NewsPageTest {
 	public static void main(String[] args) {
-		System.setProperty("http.proxyHost","10.10.10.78");
-		System.setProperty("http.proxyPort","8080");
 		
 		String url = "http://headlines.yahoo.co.jp/videonews/fnn?a=20150115-00000418-fnn-int";
 		String text_save_path = "d:\\text.txt";
